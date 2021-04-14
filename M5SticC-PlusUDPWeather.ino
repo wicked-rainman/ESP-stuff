89092cd42f2b7c59de3996c3d781243354c8406a
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2021-04-14 16:31:41 +0100

    Update M5SticC-PlusUDPWeather.ino

--- M5SticC-PlusUDPWeather.ino
+++ M5SticC-PlusUDPWeather.ino
@@ -54,65 +54,78 @@ void loop() {
   char outTemp[10];
   char outHumi[10];
   char RelPress[10];
-  float pres,baro,temp;
+  float pres, baro, temp;
   int pos;
   char mmRain[10];
   int packetSize = 0;
   int payloadSize = 0;
   char *weatherptr;
-  
   packetSize = Udp.parsePacket();
   if (packetSize) {
     payloadSize = Udp.read(weatherChars, 255);
     if (payloadSize > 0) {
       weatherChars[payloadSize] = 0;
-      weatherptr=weatherChars;
-      RelPress[0]='\0';
-      outHumi[0]='\0';
-      outTemp[0]='\0';
-      M5.Lcd.fillScreen(BLACK);
+      weatherptr = weatherChars;
+      RelPress[0] = '\0';
+      outHumi[0] = '\0';
+      outTemp[0] = '\0';
       
-      //Extract pressure
+      getLocalTime(&timeinfo);
+
+      M5.Lcd.fillScreen(BLACK);
+      //Draw a stupid frame round the values that get printed
+      M5.Lcd.drawLine(8, 8, 230, 8, GREEN); //top horizontal line
+      M5.Lcd.drawLine(8, 130, 230, 130, GREEN); //bottom horizontal line
+      M5.Lcd.drawLine(8, 45, 230, 45, GREEN); //Second horizontal line
+      M5.Lcd.drawLine(8, 82, 230, 82, GREEN); //Third horizontal line
+      M5.Lcd.drawLine(8, 8, 8, 130, GREEN); //left
+      M5.Lcd.drawLine(230, 8, 230, 130, GREEN); //right
+      M5.Lcd.drawLine(120, 45, 120, 82, GREEN); //Break between humidity and temp
+
+      //Extract and print pressure
       pos = strpos(",", weatherptr);
       strncpy(RelPress, weatherptr, pos);
       RelPress[pos] = 0;
-      weatherptr += (pos+1);
-      M5.Lcd.setCursor(40, 10);
+      weatherptr += (pos + 1);
+      M5.Lcd.setCursor(45, 14);
       pres = strtof(RelPress, NULL);
       if (pres > 1000) M5.Lcd.setTextColor(GREEN);
-        else M5.Lcd.setTextColor(ORANGE);
+      else M5.Lcd.setTextColor(ORANGE);
+      strcat(RelPress, "mb");
       M5.Lcd.print(RelPress);
-      
-      //Extract Humidity
+
+      //Extract and print Humidity
       pos = strpos(",", weatherptr);
       strncpy(outHumi, weatherptr, pos);
       outHumi[pos] = 0;
       strcat(outHumi, "%");
-      weatherptr += (pos+1);
-      M5.Lcd.setCursor(10, 50);
+      weatherptr += (pos + 1);
+      M5.Lcd.setCursor(40, 53);
       M5.Lcd.setTextColor(WHITE);
       M5.Lcd.print(outHumi);
 
       //Extract and print temp
       strcpy(outTemp, weatherptr);
-      outTemp[strlen(outTemp) - 1] = '\0';      
+      outTemp[strlen(outTemp) - 1] = '\0';
       strcat(outTemp, "C");
       temp = strtof(outTemp, NULL);
       if (temp > 24) M5.Lcd.setTextColor(RED);
       else if (temp > 10) M5.Lcd.setTextColor(GREEN);
       else if (temp > 5) M5.Lcd.setTextColor(WHITE);
       else M5.Lcd.setTextColor(BLUE);
-      M5.Lcd.setCursor(110, 50);
+      M5.Lcd.setCursor(125, 53);
       M5.Lcd.print(outTemp);
-      getLocalTime(&timeinfo);
-      
-      //Print current time
-      M5.Lcd.setCursor(40,90);
+
+      //Print time
+      M5.Lcd.setCursor(50, 95);
       M5.Lcd.setTextColor(WHITE);
       M5.Lcd.print(&timeinfo, "%H:%M:%S");
     }
   }
 }
+
+
+
 int strpos(char *needle, char *haystack) {
   char *offset;
   offset = strstr(haystack, needle);
