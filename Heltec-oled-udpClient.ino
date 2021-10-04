b3de65e5d32245cdafe71b1dad32f3acfece0e1d
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2021-10-04 11:09:57 +0100

    Update Heltec-oled-udpClient.ino

--- Heltec-oled-udpClient.ino
+++ Heltec-oled-udpClient.ino
@@ -1,21 +1,24 @@
+//------------------------------------------------------------------------------------
 //Heltec board with built in oled display
-//UDP client to diplay weather values sent from a remote esp32 with attached BME280 (Temp, pressure and humidity)
-
+//UDP client to diplay weather values sent from a remote esp32 with attached BME280 
+//(Temp, pressure and humidity). See ESP8266-bm280-weather-stn.c in this git
+//------------------------------------------------------------------------------------
 #include "Arduino.h"
 #include "heltec.h"
 #include <WiFi.h>
 #include <WiFiUdp.h>
-const char ssid[]="ssid";
-const char pass[]="pass";
+const char ssid[] = "an ssid";
+const char pass[] = "a password";
 WiFiUDP udp;
-
+//----------------------------------
+// Setup
+//----------------------------------
 void setup() {
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
   Heltec.display->flipScreenVertically();
   Heltec.display->setFont(ArialMT_Plain_16);
   Heltec.display->clear();
   Heltec.display->display();
-
   WiFi.disconnect();
   WiFi.mode(WIFI_MODE_STA);
   WiFi.begin(ssid, pass);
@@ -25,12 +28,13 @@ void setup() {
     Heltec.display->display();
   }
   Heltec.display->clear();
-  Heltec.display->drawString(0, 0, IpAddress2String(WiFi.localIP()));
+  Heltec.display->drawString(0, 5, IpAddress2String(WiFi.localIP()));
   Heltec.display->display();
   udp.begin(5679);
-
 }
-
+//-----------------------
+// Main loop
+//-----------------------
 void loop() {
   char weatherChars[1024];
   char outTemp[10];
@@ -46,39 +50,40 @@ void loop() {
     if (payloadSize > 0) {
       weatherChars[payloadSize] = 0;
       weatherptr=weatherChars;
+      Serial.println(weatherChars);
       
-      //Extract pressure
-      RelPress[0] = '\0';
-      pos = strpos(",", weatherptr);
-      strncpy(RelPress, weatherptr, pos);
-      RelPress[pos]='\0';
-      strcat(RelPress,"mb");
-      weatherptr += (pos+1);
-
-      //Extract Humidity
-      outHumi[0] = '\0';
-      pos = strpos(",", weatherptr);
-      strncpy(outHumi, weatherptr, pos);
-      outHumi[pos] = 0;
-      strcat(outHumi, "%");
+      //Jump over the Epoc date field
+      pos = strpos(" ",weatherptr);
       weatherptr += (pos+1);
 
       //Extract temp
       outTemp[0] = '\0';
-      //pos=strpos(",",weatherChars);
-      strcpy(outTemp, weatherptr);
-      outTemp[strlen(outTemp) - 1] = '\0';
+      pos=strpos(" ",weatherptr);
+      strncpy(outTemp, weatherptr,pos);
+      outTemp[pos] = '\0';
       strcat(outTemp, "c");
+      weatherptr+=(pos+1);
 
+      //Extract Humidity
+      outHumi[0] = '\0';
+      pos = strpos(" ", weatherptr);
+      strncpy(outHumi, weatherptr, pos);
+      outHumi[pos] = 0;
+      strcat(outHumi, "%");
+      weatherptr += (pos+1);
+      
+      //Extract pressure
+      RelPress[0] = '\0';
+      strcpy(RelPress, weatherptr);
+      RelPress[strlen(RelPress)]='\0';
+      strcat(RelPress,"mb");
+      
+      //Write the values to the builtin screen
       Heltec.display->clear();
-      Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
       Heltec.display->setFont(ArialMT_Plain_24);
-
-      Heltec.display->drawString(2, 5, outTemp);
-      Heltec.display->drawString(75, 5, outHumi);
-      Heltec.display->setFont(ArialMT_Plain_24);
-      Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
-      Heltec.display->drawString(55, 40, RelPress);
+      Heltec.display->drawString(2, 8, outTemp);
+      Heltec.display->drawString(75, 8, outHumi);
+      Heltec.display->drawString(5, 40, RelPress);
       Heltec.display->display();
 
     }
