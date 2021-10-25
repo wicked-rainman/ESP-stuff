748c49bf0f82d6d392e69187b0448a7bc6ec6cdd
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2021-10-25 12:58:55 +0100

    Update and rename GPS_M5Atom_Dev.c to GpsWifiUdpBridge.c

--- GpsWifiUdpBridge.c
+++ GpsWifiUdpBridge.c
@@ -1,12 +1,22 @@
+//--------------------------------------------------------------------
+// Program: GpsWifiBridge.c
+// Hardware: M5Atom lite and M5 BDS GPS on I2c
+// Purpose: Enables distribution of GPS sentences over 
+// Wifi as a UDP broadcast
+//--------------------------------------------------------------------
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <M5Atom.h>
 #define MAX_INPUT_LINE_LENGTH 150
 #define UDP_PORT 5680
+#define ORANGE 0xff9900
+#define GREEN 0x00ff00
+#define BLUE 0x0000ff
+#define WHITE 0xffffff
 WiFiUDP udp;
 IPAddress SendIP(192,168,1,255);
 const char *ssid =  "xxxxxx";
-const char *pass =  "zzzzzzz";
+const char *pass =  "xxxxxxx";
 char packet[200];
 HardwareSerial GPSRaw(2);
 char ch;
@@ -14,7 +24,7 @@ int packetSize;
 
 //--------------------------------------------------------------------
 void setup() {
-  M5.begin();
+  M5.begin(true,true,true);
   WiFi.begin(ssid,pass);
   while(WiFi.status() !=WL_CONNECTED) {
     delay(100);
@@ -32,13 +42,15 @@ void loop() {
     ch = GPSRaw.read();
     //If char is a newline, then complete sentence received.
     //Output it as a UDP broadcast
-    if (ch == '\n') {
+    if ((ch == '\n') && (packet[0] == '$')) {
       packet[packetSize]='\0';
       //Ditch any GPTXT sentences and output anything else
       if(memcmp("$GPTXT",packet,6)!=0) {
+        FlashPix(8,WHITE);
         udp.beginPacket(SendIP,UDP_PORT);
         udp.print(packet);
         udp.endPacket();
+        //Serial.println(packet);
       }
       packetSize = 0;
       memset(packet,0,200);
@@ -53,4 +65,11 @@ void loop() {
       packetSize = 0;
     }
   }
+}
+
+void FlashPix(int flashtime, int rgbcolor) {
+  M5.dis.drawpix(0, rgbcolor);
+  delay(flashtime);
+  M5.dis.clear();
+  delay(flashtime);
 }
